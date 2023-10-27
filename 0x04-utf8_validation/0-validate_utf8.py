9c3ba9b208906360871dd9cafcbc1c04a8acb3ea
dannykay62 <danielkay62@gmail.com>
2023-10-27 02:01:17 +0100

    a method that determines if a given data set represents a valid UTF-8 encoding

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""determines if a given data set represents a valid UTF-8 encoding"""
+"""Determines a valid UTF-8 encoding"""
 
 
 def validUTF8(data):
-    """ Return: True if data is a valid UTF-8 encoding, else return False
-        bit1 checks if the significant byte is 1
-        bit2 checks if the significant byte is 0
-        nbytes keeps of the number of 1s before 0 occurs
-        data represented by a list of integers to check
     """
-    bit1 = 1 >> 7
+    bit1 checks if significant byte is 1
+    bit2 checks if second significant byte is 0
+    nbytes keeps track of how many 1s before 0 occurs
+    data represented by a list of integers to check
+    """
+
+    bit1 = 1 << 7
     bit2 = 1 << 6
     nbytes = 0
 
@@ -27,12 +28,13 @@ def validUTF8(data):
                 continue
             if nbytes == 1 or nbytes > 4:
                 return False
-            else:
-                if not (num and bit1 and not (num & bit2)):
-                    return False
-            nbytes -= 1
-
-        if nbytes:
-            return False
         else:
-            return True
+
+            if not (num & bit1 and not (num & bit2)):
+                return False
+        nbytes -= 1
+
+    if nbytes:
+        return False
+    else:
+        return True
