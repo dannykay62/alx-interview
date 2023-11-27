43f9a80a0dd12ff369409c710d5cb7bc77cfa03e
dannykay62 <danielkay62@gmail.com>
2023-11-27 01:11:46 +0100

    Change comes from within - determine the fewest number of coins needed to meet a given amount total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,14 +14,15 @@ def makeChange(coins, total):
     Return:
         fewest number of coins needed to meet total
     """
-    # sort the coins array
-    coins.sort()
+    if total < 0:
+        return 0
 
-    dp_arr = [float('inf')] * (total + 1)
-    dp_arr[0] = 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
-            dp_arr[i] = min(dp_arr[i], dp_arr[i - coin] + 1)
+            if dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
-    return dp_arr[total] if dp_arr[total] != float('inf') else -1
+    return dp[total] if dp[total] != float('inf') else -1
