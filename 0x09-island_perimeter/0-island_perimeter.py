a593096dc1fd0fee0dde95869eab12ab40da11dd
dannykay62 <danielkay62@gmail.com>
2023-12-01 00:21:38 +0100

    Create a function that returns the perimeter of the island described in grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,8 +13,8 @@ def island_perimeter(grid):
         grid is rectangular, with its width and height not exceeding 100
         The grid is completely surrounded by water
         There is only one island (or nothing).
-        The island does not have “lakes” (water inside that is not connected to the
-        water surrounding the island)
+        The island does not have “lakes” (water inside that is not connected
+        to the water surrounding the island)
     """
     perimeter = 0
     for y in range(len(grid)):
